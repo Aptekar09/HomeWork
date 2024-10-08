b2bc3eb5e28c40be1159d94a95d5b5101a8b6b96
Muharbi <you@example.com>
2024-10-08 15:18:24 +0300

    add decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,29 +1,38 @@
-from typing import Optional
+
+from typing import Optional, Callable
+from functools import wraps
 
 
-def log(filename: Optional[str]):
-    def decorate(func):
+def log(filename: Optional[str]=None) -> Callable:
+    def decorate(func) -> Callable:
+        @wraps(func)
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            try:
+             try:
+                result = func(*args, **kwargs)
                 if filename:
+                    result = func(*args, **kwargs)
+                    log_message = f'{func.__name__} ок\n'
                     with open (filename, "a",encoding='utf-8') as file:
-                        file.write(f'{func.__name__} ок')
+                        file.write(f'{log_message}')
+                    return result
                 else:
-                    print(f'{func.__name__} ок')
-            except Exception as e:
+                    result = func(*args, **kwargs)
+                    log_message = f'{func.__name__} ок\n'
+                    print(f'{log_message}')
+                    return result
+             except Exception as e:
                 if filename:
+                    log_message = f'{func.__name__} error: {e.__class__.__name__} Inputs: {args}, {kwargs}\n'
                     with open(filename, 'a', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} error: {e.__class__.__name__} Inputs: {args}, {kwargs}')
+                        file.write(f'{log_message}')
                 else:
-                    print(f'{func.__name__} error: {e.__class__.__name__} Inputs: {args}, {kwargs}')
-
+                    print(f'{func.__name__} error: {e.__class__.__name__} Inputs: {args}, {kwargs}\n')
         return wrapper
     return decorate
 
 
-@log(filename="mylog.txt")
+@log()
 def my_function(x, y):
-    return x + y
+    return x / y
 
-print(my_function(1, 2))
+print(my_function(1, 0))
